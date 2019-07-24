d3f25d494cd88e24e99f19e155e38817b851d637
Colin Jaffe <balloonasaurus@gmail.com>
2019-07-24 10:18:38 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,29 @@
+// # Find the treasure!
+
+// We have found a map that has marked places that might contain the treasure we are looking for!
+
+const  places = ["Brazil","Portugal", "Peru","Spain", "Montenegro","Canada","Mexico" , "Kosovo"]
+
+// There is a secret place that wasn't in the map and it's name it starts with a `F` their known for their High Tower! Add that place to the array.
+
+
+
+// Last night someone already delivered the news that they already checked the places that start with a `P`, and the treasure is not there. Remove the places that start with a `p` from the places array. 
+
+
+// Today an ancient person found out another fact written in the the map. The place has more than 5 letters but less than 10! Using this knowledge remove the places that their length is less than 5 and more than 10.
+
+
+
+// A group checked the places left but only the ones that didn't end with an `O`. Now we know that the place where the treasure is hidden ends with an `O`, so write the code that makes the array to be left with only the places that end with an `O`.
+
+// Only a King can open the treasure because the place starts with a `K`.  Now that we know with what it starts and what it ends we can find the place. Write the code to find the place and and `console.log` that place!
+
+
+
+// You can re-assign or make new arrays on your journey! 
+
+
+
+
+// *Real life fact there is real treasure in that country!
